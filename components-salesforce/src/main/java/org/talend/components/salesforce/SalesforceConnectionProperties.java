141f768a5558ac1af0e90b6d62469990b2854ae7
pxmiao <pxmiao@talend.com>
2016-09-07 11:00:41 +0800

    TDI-37028 : Upgrade Salesforce API with latest one(should be >=37.0). https://jira.talendforge.org/browse/TDI-37028

--- components-salesforce/src/main/java/org/talend/components/salesforce/SalesforceConnectionProperties.java
+++ components-salesforce/src/main/java/org/talend/components/salesforce/SalesforceConnectionProperties.java
@@ -13,7 +13,10 @@
 package org.talend.components.salesforce;
 
 import static org.talend.daikon.properties.presentation.Widget.widget;
-import static org.talend.daikon.properties.property.PropertyFactory.*;
+import static org.talend.daikon.properties.property.PropertyFactory.newBoolean;
+import static org.talend.daikon.properties.property.PropertyFactory.newEnum;
+import static org.talend.daikon.properties.property.PropertyFactory.newInteger;
+import static org.talend.daikon.properties.property.PropertyFactory.newString;
 
 import org.talend.components.api.exception.ComponentException;
 import org.talend.components.api.properties.ComponentPropertiesImpl;
@@ -30,214 +33,216 @@ import org.talend.daikon.properties.presentation.Widget;
 import org.talend.daikon.properties.property.Property;
 
 public class SalesforceConnectionProperties extends ComponentPropertiesImpl
-        implements SalesforceProvideConnectionProperties, ComponentReferencePropertiesEnclosing {
+		implements SalesforceProvideConnectionProperties, ComponentReferencePropertiesEnclosing {
 
-    public static final String URL = "https://www.salesforce.com/services/Soap/u/34.0";
+	public static final String URL = "https://www.salesforce.com/services/Soap/u/37.0";
 
-    public static final String OAUTH_URL = "https://login.salesforce.com/services/oauth2";
+	public static final String OAUTH_URL = "https://login.salesforce.com/services/oauth2";
 
-    public Property<String> endpoint = newString("endpoint").setRequired();
+	public Property<String> endpoint = newString("endpoint").setRequired();
 
-    public static final String FORM_WIZARD = "Wizard";
+	public static final String FORM_WIZARD = "Wizard";
 
-    //
-    // Properties
-    //
+	//
+	// Properties
+	//
 
-    // Only for the wizard use
-    public Property<String> name = newString("name").setRequired();
+	// Only for the wizard use
+	public Property<String> name = newString("name").setRequired();
 
-    public enum LoginType {
-        Basic,
-        OAuth
-    }
+	public enum LoginType {
+		Basic, OAuth
+	}
 
-    public Property<LoginType> loginType = newEnum("loginType", LoginType.class).setRequired();
+	public Property<LoginType> loginType = newEnum("loginType", LoginType.class).setRequired();
 
-    public Property<Boolean> bulkConnection = newBoolean("bulkConnection"); //$NON-NLS-1$
+	public Property<Boolean> bulkConnection = newBoolean("bulkConnection"); //$NON-NLS-1$
 
-    public Property<Boolean> needCompression = newBoolean("needCompression"); //$NON-NLS-1$
+	public Property<Boolean> needCompression = newBoolean("needCompression"); //$NON-NLS-1$
 
-    public Property<Integer> timeout = newInteger("timeout"); //$NON-NLS-1$
+	public Property<Integer> timeout = newInteger("timeout"); //$NON-NLS-1$
 
-    public Property<Boolean> httpTraceMessage = newBoolean("httpTraceMessage"); //$NON-NLS-1$
+	public Property<Boolean> httpTraceMessage = newBoolean("httpTraceMessage"); //$NON-NLS-1$
 
-    public Property<Boolean> httpChunked = newBoolean("httpChunked"); //$NON-NLS-1$
+	public Property<Boolean> httpChunked = newBoolean("httpChunked"); //$NON-NLS-1$
 
-    public Property<String> clientId = newString("clientId"); //$NON-NLS-1$
+	public Property<String> clientId = newString("clientId"); //$NON-NLS-1$
 
-    //
-    // Presentation items
-    //
-    public PresentationItem testConnection = new PresentationItem("testConnection", "Test connection");
+	//
+	// Presentation items
+	//
+	public PresentationItem testConnection = new PresentationItem("testConnection", "Test connection");
 
-    public PresentationItem advanced = new PresentationItem("advanced", "Advanced...");
+	public PresentationItem advanced = new PresentationItem("advanced", "Advanced...");
 
-    //
-    // Nested property collections
-    //
-    private static final String OAUTH = "oauth";
+	//
+	// Nested property collections
+	//
+	private static final String OAUTH = "oauth";
 
-    public OauthProperties oauth = new OauthProperties(OAUTH);
+	public OauthProperties oauth = new OauthProperties(OAUTH);
 
-    private static final String USERPASSWORD = "userPassword";
+	private static final String USERPASSWORD = "userPassword";
 
-    public SalesforceUserPasswordProperties userPassword = new SalesforceUserPasswordProperties(USERPASSWORD);
+	public SalesforceUserPasswordProperties userPassword = new SalesforceUserPasswordProperties(USERPASSWORD);
 
-    public ProxyProperties proxy = new ProxyProperties("proxy");
+	public ProxyProperties proxy = new ProxyProperties("proxy");
 
-    public ComponentReferenceProperties referencedComponent = new ComponentReferenceProperties("referencedComponent", this);
+	public ComponentReferenceProperties referencedComponent = new ComponentReferenceProperties("referencedComponent",
+			this);
 
-    public SalesforceConnectionProperties(String name) {
-        super(name);
-    }
+	public SalesforceConnectionProperties(String name) {
+		super(name);
+	}
 
-    @Override
-    public void setupProperties() {
-        super.setupProperties();
+	@Override
+	public void setupProperties() {
+		super.setupProperties();
 
-        loginType.setValue(LoginType.Basic);
-        endpoint.setValue(URL);
-        timeout.setValue(60000);
-        httpChunked.setValue(true);
-    }
+		loginType.setValue(LoginType.Basic);
+		endpoint.setValue(URL);
+		timeout.setValue(60000);
+		httpChunked.setValue(true);
+	}
 
-    @Override
-    public void setupLayout() {
-        super.setupLayout();
+	@Override
+	public void setupLayout() {
+		super.setupLayout();
 
-        Form wizardForm = Form.create(this, FORM_WIZARD);
-        wizardForm.addRow(name);
-        wizardForm.addRow(widget(loginType).setWidgetType(Widget.ENUMERATION_WIDGET_TYPE).setDeemphasize(true));
-        wizardForm.addRow(oauth.getForm(Form.MAIN));
-        wizardForm.addRow(userPassword.getForm(Form.MAIN));
-        wizardForm.addRow(widget(advanced).setWidgetType(Widget.BUTTON_WIDGET_TYPE));
-        wizardForm.addColumn(widget(testConnection).setLongRunning(true).setWidgetType(Widget.BUTTON_WIDGET_TYPE));
+		Form wizardForm = Form.create(this, FORM_WIZARD);
+		wizardForm.addRow(name);
+		wizardForm.addRow(widget(loginType).setWidgetType(Widget.ENUMERATION_WIDGET_TYPE).setDeemphasize(true));
+		wizardForm.addRow(oauth.getForm(Form.MAIN));
+		wizardForm.addRow(userPassword.getForm(Form.MAIN));
+		wizardForm.addRow(widget(advanced).setWidgetType(Widget.BUTTON_WIDGET_TYPE));
+		wizardForm.addColumn(widget(testConnection).setLongRunning(true).setWidgetType(Widget.BUTTON_WIDGET_TYPE));
 
-        Form mainForm = Form.create(this, Form.MAIN);
-        mainForm.addRow(widget(loginType).setWidgetType(Widget.ENUMERATION_WIDGET_TYPE));
-        mainForm.addRow(oauth.getForm(Form.MAIN));
-        mainForm.addRow(userPassword.getForm(Form.MAIN));
+		Form mainForm = Form.create(this, Form.MAIN);
+		mainForm.addRow(widget(loginType).setWidgetType(Widget.ENUMERATION_WIDGET_TYPE));
+		mainForm.addRow(oauth.getForm(Form.MAIN));
+		mainForm.addRow(userPassword.getForm(Form.MAIN));
 
-        Form advancedForm = Form.create(this, Form.ADVANCED);
-        advancedForm.addRow(endpoint);
-        advancedForm.addRow(bulkConnection);
-        advancedForm.addRow(needCompression);
-        advancedForm.addRow(httpTraceMessage);
-        advancedForm.addRow(httpChunked);
-        advancedForm.addRow(clientId);
-        advancedForm.addRow(timeout);
-        advancedForm.addRow(proxy.getForm(Form.MAIN));
-        advanced.setFormtoShow(advancedForm);
+		Form advancedForm = Form.create(this, Form.ADVANCED);
+		advancedForm.addRow(endpoint);
+		advancedForm.addRow(bulkConnection);
+		advancedForm.addRow(needCompression);
+		advancedForm.addRow(httpTraceMessage);
+		advancedForm.addRow(httpChunked);
+		advancedForm.addRow(clientId);
+		advancedForm.addRow(timeout);
+		advancedForm.addRow(proxy.getForm(Form.MAIN));
+		advanced.setFormtoShow(advancedForm);
 
-        // A form for a reference to a connection, used in a tSalesforceInput for example
-        Form refForm = Form.create(this, Form.REFERENCE);
-        Widget compListWidget = widget(referencedComponent).setWidgetType(Widget.COMPONENT_REFERENCE_WIDGET_TYPE);
-        referencedComponent.componentType.setValue(TSalesforceConnectionDefinition.COMPONENT_NAME);
-        refForm.addRow(compListWidget);
-        refForm.addRow(mainForm);
-    }
+		// A form for a reference to a connection, used in a tSalesforceInput
+		// for example
+		Form refForm = Form.create(this, Form.REFERENCE);
+		Widget compListWidget = widget(referencedComponent).setWidgetType(Widget.COMPONENT_REFERENCE_WIDGET_TYPE);
+		referencedComponent.componentType.setValue(TSalesforceConnectionDefinition.COMPONENT_NAME);
+		refForm.addRow(compListWidget);
+		refForm.addRow(mainForm);
+	}
 
-    public void afterLoginType() {
-        refreshLayout(getForm(Form.MAIN));
-        refreshLayout(getForm(FORM_WIZARD));
-        refreshLayout(getForm(Form.ADVANCED));
-    }
+	public void afterLoginType() {
+		refreshLayout(getForm(Form.MAIN));
+		refreshLayout(getForm(FORM_WIZARD));
+		refreshLayout(getForm(Form.ADVANCED));
+	}
 
-    @Override
-    public void afterReferencedComponent() {
-        refreshLayout(getForm(Form.MAIN));
-        refreshLayout(getForm(Form.REFERENCE));
-        refreshLayout(getForm(Form.ADVANCED));
-    }
+	@Override
+	public void afterReferencedComponent() {
+		refreshLayout(getForm(Form.MAIN));
+		refreshLayout(getForm(Form.REFERENCE));
+		refreshLayout(getForm(Form.ADVANCED));
+	}
 
-    public void afterBulkConnection() {
-        refreshLayout(getForm(Form.ADVANCED));
-    }
+	public void afterBulkConnection() {
+		refreshLayout(getForm(Form.ADVANCED));
+	}
 
-    public ValidationResult validateTestConnection() throws Exception {
-        ValidationResult vr = SalesforceSourceOrSink.validateConnection(this);
-        if (vr.getStatus() == ValidationResult.Result.OK) {
-            vr.setMessage("Connection successful");
-            getForm(FORM_WIZARD).setAllowForward(true);
-        } else {
-            getForm(FORM_WIZARD).setAllowForward(false);
-        }
-        return vr;
-    }
+	public ValidationResult validateTestConnection() throws Exception {
+		ValidationResult vr = SalesforceSourceOrSink.validateConnection(this);
+		if (vr.getStatus() == ValidationResult.Result.OK) {
+			vr.setMessage("Connection successful");
+			getForm(FORM_WIZARD).setAllowForward(true);
+		} else {
+			getForm(FORM_WIZARD).setAllowForward(false);
+		}
+		return vr;
+	}
 
-    @Override
-    public void refreshLayout(Form form) {
-        super.refreshLayout(form);
+	@Override
+	public void refreshLayout(Form form) {
+		super.refreshLayout(form);
 
-        String refComponentIdValue = getReferencedComponentId();
-        boolean useOtherConnection = refComponentIdValue != null
-                && refComponentIdValue.startsWith(TSalesforceConnectionDefinition.COMPONENT_NAME);
-        if (form.getName().equals(Form.MAIN) || form.getName().equals(FORM_WIZARD)) {
-            if (useOtherConnection) {
-                form.getWidget(loginType.getName()).setHidden(true);
-                form.getWidget(OAUTH).setHidden(true);
-                form.getWidget(USERPASSWORD).setHidden(true);
-            } else {
-                form.getWidget(loginType.getName()).setHidden(false);
-                String endpointValue = endpoint.getValue();
-                switch (loginType.getValue()) {
-                case Basic:
-                    form.getWidget(OAUTH).setHidden(true);
-                    form.getWidget(USERPASSWORD).setHidden(false);
-                    if (endpointValue == null || endpointValue.contains(OAUTH_URL)) {
-                        endpoint.setValue(URL);
-                    }
-                    break;
-                case OAuth:
-                    form.getWidget(OAUTH).setHidden(false);
-                    form.getWidget(USERPASSWORD).setHidden(true);
-                    if (endpointValue == null || endpointValue.contains(URL)) {
-                        endpoint.setValue(OAUTH_URL);
-                    }
-                    break;
+		String refComponentIdValue = getReferencedComponentId();
+		boolean useOtherConnection = refComponentIdValue != null
+				&& refComponentIdValue.startsWith(TSalesforceConnectionDefinition.COMPONENT_NAME);
+		if (form.getName().equals(Form.MAIN) || form.getName().equals(FORM_WIZARD)) {
+			if (useOtherConnection) {
+				form.getWidget(loginType.getName()).setHidden(true);
+				form.getWidget(OAUTH).setHidden(true);
+				form.getWidget(USERPASSWORD).setHidden(true);
+			} else {
+				form.getWidget(loginType.getName()).setHidden(false);
+				String endpointValue = endpoint.getValue();
+				switch (loginType.getValue()) {
+				case Basic:
+					form.getWidget(OAUTH).setHidden(true);
+					form.getWidget(USERPASSWORD).setHidden(false);
+					if (endpointValue == null || endpointValue.contains(OAUTH_URL)) {
+						endpoint.setValue(URL);
+					}
+					break;
+				case OAuth:
+					form.getWidget(OAUTH).setHidden(false);
+					form.getWidget(USERPASSWORD).setHidden(true);
+					if (endpointValue == null || endpointValue.contains(URL)) {
+						endpoint.setValue(OAUTH_URL);
+					}
+					break;
 
-                default:
-                    throw new ComponentException(new Throwable("Enum value should be handled :" + loginType.getValue()));
-                }
-            }
-        }
+				default:
+					throw new ComponentException(
+							new Throwable("Enum value should be handled :" + loginType.getValue()));
+				}
+			}
+		}
 
-        if (form.getName().equals(Form.ADVANCED)) {
-            if (useOtherConnection) {
-                form.setHidden(true);
-            } else {
-                form.setHidden(false);
+		if (form.getName().equals(Form.ADVANCED)) {
+			if (useOtherConnection) {
+				form.setHidden(true);
+			} else {
+				form.setHidden(false);
 
-                boolean bulkMode = bulkConnection.getValue();
-                form.getWidget(httpChunked.getName()).setHidden(bulkMode);
-                form.getWidget(httpTraceMessage.getName()).setHidden(!bulkMode);
+				boolean bulkMode = bulkConnection.getValue();
+				form.getWidget(httpChunked.getName()).setHidden(bulkMode);
+				form.getWidget(httpTraceMessage.getName()).setHidden(!bulkMode);
 
-                Form proxyForm = form.getChildForm(proxy.getName());
-                if (proxyForm != null) {
-                    boolean isUseProxy = proxy.useProxy.getValue();
-                    proxyForm.getWidget(proxy.host.getName()).setHidden(!isUseProxy);
-                    proxyForm.getWidget(proxy.port.getName()).setHidden(!isUseProxy);
-                    proxyForm.getWidget(proxy.userPassword.getName()).setHidden(!isUseProxy);
-                }
-            }
-        }
-    }
+				Form proxyForm = form.getChildForm(proxy.getName());
+				if (proxyForm != null) {
+					boolean isUseProxy = proxy.useProxy.getValue();
+					proxyForm.getWidget(proxy.host.getName()).setHidden(!isUseProxy);
+					proxyForm.getWidget(proxy.port.getName()).setHidden(!isUseProxy);
+					proxyForm.getWidget(proxy.userPassword.getName()).setHidden(!isUseProxy);
+				}
+			}
+		}
+	}
 
-    @Override
-    public SalesforceConnectionProperties getConnectionProperties() {
-        return this;
-    }
+	@Override
+	public SalesforceConnectionProperties getConnectionProperties() {
+		return this;
+	}
 
-    public String getReferencedComponentId() {
-        return referencedComponent.componentInstanceId.getStringValue();
-    }
+	public String getReferencedComponentId() {
+		return referencedComponent.componentInstanceId.getStringValue();
+	}
 
-    public SalesforceConnectionProperties getReferencedConnectionProperties() {
-        SalesforceConnectionProperties refProps = (SalesforceConnectionProperties) referencedComponent.componentProperties;
-        if (refProps != null) {
-            return refProps;
-        }
-        return null;
-    }
+	public SalesforceConnectionProperties getReferencedConnectionProperties() {
+		SalesforceConnectionProperties refProps = (SalesforceConnectionProperties) referencedComponent.componentProperties;
+		if (refProps != null) {
+			return refProps;
+		}
+		return null;
+	}
 }
